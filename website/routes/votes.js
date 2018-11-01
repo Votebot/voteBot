4dceb3bc16233127f2583d5e070ef05d73e14487
Oskar Lang <oskar.risto.lang@gmail.com>
2018-11-02 00:41:09 +0100

    Added UserInfo

--- website/routes/votes.js
+++ website/routes/votes.js
@@ -2,11 +2,11 @@ let express = require('express');
 let router = express.Router();
 let superagent = require('superagent');
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.redirect('/');
 });
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', function (req, res) {
     let voteId = req.params.id;
 
     superagent.get(`http://localhost:8080/votes/${voteId}`).then(data => {
@@ -24,19 +24,30 @@ router.get('/:id', function (req, res, next) {
                 count: data.body.answers[option],
                 emote: emotes[option]
             };
-
-            console.log(obj);
-
             votes.push(obj);
         });
 
         let obj = {
             voteTitle: data.body.heading,
             voteHeading: data.body.heading,
-            votes: votes
+            votes: votes,
+            user: {}
         };
 
-        res.render('votes', obj);
+        let identifier = '254892085000405004'; // TODO: FIX ID
+
+        superagent.get(`https://discordapp.com/api/users/${identifier}`)
+            .set('Authorization', 'Bot NTA3NjA4NTIyMzk4MTA1NjEw.DrzK9w.WCiL6OZDWwLjSFzog8wWNaolUZQ')
+            .then(discord_data => {
+                console.log(discord_data.body);
+                obj.user.avatar = `https://cdn.discordapp.com/avatars/${identifier}/${discord_data.body.avatar}.png`;
+                obj.user.name = discord_data.body.username;
+                obj.user.discriminator = discord_data.body.discriminator;
+                obj.user.id = discord_data.body.id;
+                res.render('votes', obj);
+            }).catch(error => {
+            console.log(error);
+        });
     }).catch(err => {
         console.log(err);
         res.redirect('/');
