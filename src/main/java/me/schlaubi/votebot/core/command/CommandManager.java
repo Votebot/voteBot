9e4d529d32434a5268d4087f992a683d7c08b356
Michael R.|SchlaumeierTV <mail@schlaubi.me>
2018-11-08 19:12:25 +0100

    Fixed permission issue

--- src/main/java/me/schlaubi/votebot/core/command/CommandManager.java
+++ src/main/java/me/schlaubi/votebot/core/command/CommandManager.java
@@ -109,7 +109,7 @@ public class CommandManager implements Closeable {
             //Send typing
             channel.sendTyping().queue();
             //Delete invoke message
-            if (commandEvent.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_MANAGE))
+            if (commandEvent.getGuild().getSelfMember().hasPermission(commandEvent.getChannel(), Permission.MESSAGE_MANAGE))
                 commandEvent.getMessage().delete().queue();
             //Run the commands run method
             Result result = command.run(commandEvent.getArgs(), commandEvent);
